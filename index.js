54db5a34d34ffba1928a3fece97fb13770158589
Zane Stock <zanestock3@gmail.com>
2022-05-12 16:34:23 -0500

    commit tree fd4d77ba7004603477dbee6e87755878000f6151

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const block = 42;
+function distanceFromHqInBlocks(blockNumber) {
+    return Math.abs(blockNumber - block);
+}
+
+function distanceFromHqInFeet(feet) {
+    return (distanceFromHqInBlocks(feet)*264);
+}
+function distanceTravelledInFeet(num1, num2) {
+    let blocks;
+    blocks = (num2 - num1) * 264;
+    return Math.abs(blocks);
+}
+
+function calculatesFarePrice(num1, num2) {
+    if (num2 - num1 <= 400) 
+    return 0; 
+    else if (num2);
+    return (num2 * 0.02);
+    }
+
